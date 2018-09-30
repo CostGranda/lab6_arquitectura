b4fef5c0b1aeb6e0bc3f55ef7b032fa7302fd1fa
JL Granda <jorge_granda82112@elpoli.edu.co>
2018-09-30 11:42:41 -0500

    pruebaaa de peticion

--- app.py
+++ app.py
@@ -1,28 +1,53 @@
 from flask import Flask, render_template, request
 # Habilitar solo para probar en la raspberry
-# import RPi.GPIO as GPIO 
+# import RPi.GPIO as GPIO
+import servo
 
 APP = Flask(__name__)
-
-def contact():
-    if "nose" in request.data:
-        print("Noseee")
 
 @APP.route('/')
 def index():
     """Ruta principal
-    
+
     Returns:
         LED -- Encendido/apagado
     """
-
-
-    print("Print request form", request.form)
-    if "Right" in request.form:
-        print("Derechaaa")
-    elif "Up" in request.form:
-        print("Arribaaa")
     return render_template('home.html')
+
+
+@APP.route('/right', methods=['POST'])
+def right():
+    """Ruta principal
+
+    Returns:
+        LED -- Encendido/apagado
+    """
+    servo.correr()
+
+    print("Derecha")
+    return render_template('home.html')
+
+
+@APP.route('/center', methods=["POST"])
+def center():
+    """Ruta principal
+
+    Returns:
+        LED -- Encendido/apagado
+    """
+    print("CENTROO")
+    return render_template('home.html')
+
+
+@APP.route('/left', methods=["POST"])
+def left():
+    """Ruta principal
+
+    Returns:
+        LED -- Encendido/apagado
+    """
+    print("IZQUIERDA")
+    return 'I'
 
 
 if __name__ == '__main__':
