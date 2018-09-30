a732912c41305c1dbea70e3be36c26d1c3dcabd1
JL Granda <jorge_granda82112@elpoli.edu.co>
2018-09-30 11:10:55 -0500

    a probar codigo

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+# Habilitar solo para probar en la raspberry
+# import RPi.GPIO as GPIO 
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route('/')
+def contact():
+    if "nose" in request.data:
+        print("Noseee")
+
+@APP.route('/')
 def index():
-    return 'Hello '
+    """Ruta principal
+    
+    Returns:
+        LED -- Encendido/apagado
+    """
+
+
+    print("Print request form", request.form)
+    if "Right" in request.form:
+        print("Derechaaa")
+    elif "Up" in request.form:
+        print("Arribaaa")
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)    
-    
-    
-    
+    APP.run(host='0.0.0.0', debug=True)
